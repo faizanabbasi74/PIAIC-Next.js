1c9412139b7fee60ff609c88458b517d58d63fc8
Faizan Abbasi <faizan.74.abbasi@gmail.com>
2023-03-12 11:29:43 +0500

    Step 00

---
+++ step_00_helloworld/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+// import styles from './page.module.css'
+
+export default function Home() {
+  return (
+          <div>Hello World</div>
+  )
+}
